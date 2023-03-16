5629f0635a08d2e00dcc0f1384caaad3cd4bdf26
Anujmoglix <anuj.gupta@moglix.com>
2023-03-16 15:03:17 +0530

    calendar multiple

---
+++ src/component/EventList.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import {StyleSheet, View,Text,Modal, Platform} from "react-native";
+import Colors from "../Theme/Colors";
+import Dimension from "../Theme/Dimension";
+
+const EventList = (props) => {
+    return (
+        <View >
+            <Text>EventList</Text>
+            <Text>EvsfsdfddentList</Text>
+        </View>
+    )
+}
+
+export default EventList;
--- src/containers/Calendar/index.js
+++ src/containers/Calendar/index.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text,TouchableOpacity,Button } from "react-native";
 import Calendars from "../../component/Calendars";
+import EventList from "../../component/EventList";
+import styles from './style';
+import Dimension from "../../Theme/Dimension";
 
 const CalendarScreen = () => {
   return (
+    <View
+      style={{
+        flex: 1,
+        marginTop: Dimension.margin40,
+        backgroundColor: "#fff",
+      }}
+    >
+    <View style={styles.headerWrap}>
+    <View style={styles.TopHeader}>
+      <Text style={styles.headingTxt}>Calendar</Text>
+    </View>
+    <View>
+      <Button title="Cal" />
+      <Button title="List" />
+    </View>
+  </View>
     <View>
       <Calendars></Calendars>
+      <EventList></EventList>
+    </View>
     </View>
   );
 };
