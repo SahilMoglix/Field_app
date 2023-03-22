0c4036439d6d4a178deb4859d8204d7ddd65ee66
Moglix <moglix@MOGNOILTM1284.local>
2023-03-22 14:52:30 +0530

    [check for existing number in system added]

--- src/services/constant.js
+++ src/services/constant.js
@@ -3,22 +3,22 @@ const QA = '';
 const PROD = '';
 
 const CONSTANTS = {
-  BASE_URL = MOCK,
-  API_BASE_URL: "https://sales-funnel.moglilabs.com",
+  BASE_URL: MOCK,
+  API_BASE_URL: 'https://sales-funnel.moglilabs.com',
   API_URL: {
-    USER_DETAIL: "/api/user/mobile/",
-    CONTACT_LIST: "/api/user/contacts/",
-    NOTES_LIST: "/api/user/",
-    CHANGE_FAV: "/api/user/favorites",
-    ADD_NOTES: "/api/user/addNotes",
-    ADD_EVENT: "/api/user/addEvents",
-    ADD_SINGLE_EVENT: "/api/user/addOneEvent",
-    ADD_CONTACT: "/api/contact/save",
-    RECENT_ACTIVITY: "/api/user/activity/",
-    USERS_BY_USER_ID: "/api/user/users/",
-    CONTACT_PIC_VIEW: "/viewContactPicture?download=0"
+    USER_DETAIL: '/api/user/mobile/',
+    CONTACT_LIST: '/api/user/contacts/',
+    NOTES_LIST: '/api/user/',
+    CHANGE_FAV: '/api/user/favorites',
+    ADD_NOTES: '/api/user/addNotes',
+    ADD_EVENT: '/api/user/addEvents',
+    ADD_SINGLE_EVENT: '/api/user/addOneEvent',
+    ADD_CONTACT: '/api/contact/save',
+    RECENT_ACTIVITY: '/api/user/activity/',
+    USERS_BY_USER_ID: '/api/user/users/',
+    CONTACT_PIC_VIEW: '/viewContactPicture?download=0',
   },
-  TOKEN: "abcd",
+  TOKEN: 'abcd',
 };
 
 export default CONSTANTS;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -11,3 +11,15 @@ export const getContacts = async () =>
       'Content-Type': 'application/json',
     },
   });
+
+export const getNumberDetails = async phone =>
+  axios.get(`${CONSTANTS.BASE_URL}/searchContactByPhoneAndKAMId`, {
+    params: {
+      id: 1, //await AsyncStorage.getItem('userId')
+      phone,
+    },
+    headers: {
+      // 'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
+  });
