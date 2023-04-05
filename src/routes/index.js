ee7c26363d9ac64abbff623a39ecb9e5bced2e81
SahilMoglix <sahil.saxena@moglix.com>
2023-04-05 15:27:56 +0530

    [removed unnecessary code]

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ import {
   Platform,
   View,
   ActivityIndicator,
-  Alert,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -21,7 +20,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {fetchedAuth} from '../redux/actions/auth';
 import firebase from '@react-native-firebase/app';
 import messaging from '@react-native-firebase/messaging';
-import {Adjust, AdjustEvent, AdjustConfig} from 'react-native-adjust';
+
 const AppStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
@@ -45,19 +44,13 @@ const horizontalAnimation = {
 const getFcmToken = async () => {
   try {
     const token = await firebase.messaging().getToken();
-    Adjust.setPushToken(token);
-    console.log('fcmToken', token);
     AsyncStorage.setItem('fcm_token', token);
-    // dispatch(setFcmToken(token));
   } catch (error) {
     console.log(error);
   }
 };
 const onTokenRefreshListener = async () => {
   try {
-    const token = await firebase.messaging().onTokenRefresh();
-    Adjust.setPushToken(token);
-    console.log('fcmToken', token);
   } catch (error) {
     console.log(error);
   }
@@ -79,18 +72,18 @@ const Routes = props => {
   }, []);
   useEffect(() => {
     getFcmToken();
-
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert('A new FCM message arrived!' + JSON.stringify(remoteMessage));
+      // Alert('A new FCM message arrived!' + JSON.stringify(remoteMessage));
     });
     // Register background handler
     messaging().setBackgroundMessageHandler(async remoteMessage => {
-      console.log('Message handled in the background!', remoteMessage);
+      // console.log('Message handled in the background!', remoteMessage);
     });
     return () => {
       onTokenRefreshListener();
     };
   }, []);
+
   const init = async () => {
     let userData = await AsyncStorage.getItem('@microsoftLogin');
     let parsedToken = JSON.parse(userData);
